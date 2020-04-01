96bde28fae03d88f6ccb845d414b95ffd413ba19
Youssef <theyous@gmail.com>
2020-04-01 14:32:55 -0500

    Initial commit

---
+++ src/Ant.java
@@ -0,0 +1,23 @@
+public class Ant {
+
+    int id;
+    int lifeSpan;
+    int turn;
+    int age;
+    boolean alive;
+
+    public Ant() {
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge() {
+
+    }
+
+
+
+}
